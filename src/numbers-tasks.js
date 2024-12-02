0951aba2a0e5bb488af53c60dc536b96feebc161
Tanja Kiseleva <lessy_92@mail.ru>
2024-12-02 08:16:29 +0100

    fix: plus few tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -166,9 +166,11 @@ parseNumberFromString('-525.5');
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
+
+getParallelepipedDiagonal(1, 1, 1);
 
 /**
  * Returns the number rounded to specified power of 10.
@@ -187,9 +189,12 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const factor = 10 ** pow;
+  return Math.round(num / factor) * factor;
 }
+
+roundToPowerOfTen(1234, 0);
 
 /**
  * Returns true is the number is prime; otherwise false.
@@ -208,9 +213,15 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
+
+isPrime(2);
 
 /**
  * Tries to convert value to number and returns it if conversion was successful;
